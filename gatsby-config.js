a11f0fac9dab268a73b50060bd72a2a390e2c4dc
Joseph Jin <jerkjoe61@gmail.com>
2019-05-10 20:47:40 -0700

    homepage change

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ var proxy = require('http-proxy-middleware')
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: '前端大佬们',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      '这是一个萌新们踏上前端大佬之路的记录小站...',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
